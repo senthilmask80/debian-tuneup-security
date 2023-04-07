e4e4b18239f9fce881c7d2b51d433b16f412860a
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2023-04-07 10:59:22 +0530

    Update beginstall.sh

--- beginstall.sh
+++ beginstall.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 sudo apt update; sudo apt -y upgrade; sudo apt -y dist-upgrade;
-sudo apt -y build-essential apt-transport-https software-properties-common git git-all curl wget;
+# sudo nano /etc/apt/sources.list or /etc/apt/sources.list.d/official-debian.list  tobe add main contrib non-free
+sudo apt -y make build-essential apt-transport-https software-properties-common git git-all curl wget;
+sudo apt install -y linux-headers-6.0.0-0.deb11.6-amd64;
+sudo apt install -y linux-headers-6.0.0-0.deb11.6-common;
+sudo apt install -y linux-image-6.0.0-0.deb11.6-amd64;
+sudo update-initramfs -u
+
+//*** Python Installation with environment settings ***//
+sudo apt install -y python-is-python3 python3-full python3-pip python3-venv python3-virtualenv pipx python3-poetry-core;
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-devD;
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+curl -sSL https://install.python.poetry.org | python3 -
+pip install pip --upgrade;
+
+//*** Java Installation with environment settings ***//
+sudo apt install openjdk-17-jdk openjdk-17-jre;
+
+//*** NVM NodeJS NPM with environment settings ***//
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+nvm install node --lts;
+node install npm -g;
+npm install npm@latest -g;
+npm update -g;
+
+//*** Rust Installation with environment settings ***//
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+//*** Ruby Installation ***//
+sudo apt install ruby ruby-dev ruby-full ruby-debian rubygems-integration;
